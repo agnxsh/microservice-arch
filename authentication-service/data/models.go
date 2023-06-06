c24bde095dcb06e52728abe0517a9831695046c0
agnxsh <iamagnix@gmail.com>
2023-06-06 21:58:34 +0530

    chore (models): complete

--- authentication-service/data/models.go
+++ authentication-service/data/models.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"log"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const dbTimeout = time.Second * 3
@@ -25,7 +27,7 @@ func New (dbPool *sql.DB) Models {
 //in this type is available to us throughout the application, anywhere that the app variable is used
 //provided that the model is also added in the New function
 type Models struct {
-	User user
+	User User
 }
 
 type User struct {
@@ -45,10 +47,10 @@ func (u *User) GetAll() ([]*User, error){
 
 	defer cancel()
 
-	query:= `select id, email, first_name, last_name, password, user_active, created_at, updated_at 
+	query:= `select id, email, first_name, last_name, password, user_active, created_at, updated_at from
 	from users order by last_name`
 
-	rows, err:= db.QueryContext((ctx,query))
+	rows, err:= db.QueryContext(ctx,query)
 	if err!= nil {
 		return nil, err
 	}
@@ -78,9 +80,185 @@ func (u *User) GetAll() ([]*User, error){
 }
 
 //GetByEmail returns one user by email
-func (u *User) GetByEmail (email string) (*User, error)[
-	ctx, cancel := context.WithTimeout(context.Background, dbTimeout)
+func (u *User) GetByEmail (email string) (*User, error){
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
 
-	query :=  `select id, email, first_name, last_name, password, user_active, created_at, updated_at`
-]
+	query :=  `select id, email, first_name, last_name, password, user_active, created_at, updated_at from 
+	users order by email`
+
+	var user User
+	row:= db.QueryRowContext(ctx, query, email)
+
+	err:= row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+//GetOne returns one user by id
+func(u *User) GetOne(id int) (*User, error){
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, user_active, created_at, updated_at from users`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, id)
+
+	err:= row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+//Updating user details
+func (u *User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(),dbTimeout)
+	defer cancel()
+
+	stmt := `update users set
+		email = $1,
+		first_name = $2,
+		last_name = $3,
+		users_active = $4,
+		updated_at = $5,
+		where_id = $6`
+
+	_, err := db.ExecContext(ctx, stmt,
+	
+		u.Email,
+		u.FirstName,
+		u.LastName,
+		u.Active,
+		time.Now(),
+		u.ID,
+	)
+
+	if err!= nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) Delete() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from users where id = $1`
+
+	_, err:= db.ExecContext(ctx, stmt, u.ID)
+	if err!= nil {
+		return err
+	}
+	return nil
+}
+
+//DeleteByID deletes one user from the database, by ID
+func (u *User) DeleteByID (id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `Delete from users where id = $1`
+
+	_, err := db.ExecContext(ctx, stmt, id)
+	if err!=nil {
+		return err
+	}
+	return nil
+}
+
+
+//Insert inserts a new user into the database, and returns the ID of the newly inserted row
+func (u *User) Insert(user User) (int, error){
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+
+	defer cancel()
+
+	hashedPassword, err:= bcrypt.GenerateFromPassword([] byte (user.Password), 12)
+	if err!=nil{
+		return 0, err
+	}
+
+	var newID int
+	stmt := `insert into users (email, first_name, last_name, password, user_active, created_at,
+		updated_at) having values ($1, $2, $3, $4, $5, $6, $7) returning id`
+
+	err = db.QueryRowContext(ctx, stmt, 
+		user.Email,
+		user.FirstName,
+		user.LastName,
+		hashedPassword,
+		user.Active,
+		time.Now(),
+		time.Now(),
+
+	).Scan(&newID)
+
+	if err!= nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+//Reseting the password 
+func (u *User) ResetPassword (password string) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err!= nil{
+		return err
+	}
+
+	stmt := `update users set password = $1 where id = $2`
+	_,err = db.ExecContext(ctx, stmt, hashedPassword, u.ID)
+	if err!=nil {
+		return err
+	}
+	return nil
+}
+
+//Func call when the password matches
+func (u *User) PasswordMatches(plainText string) (bool, error){
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plainText))
+	if err!=nil {
+		swtich {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			//in case of invalid password
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+

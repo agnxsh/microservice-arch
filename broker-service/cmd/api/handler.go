77d8afcd57d0189e38ef8d514dfbb41342bd8ae5
agnxsh <iamagnix@gmail.com>
2023-06-14 12:05:13 +0530

    fix: authpayload added in broker-service handler

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -4,6 +4,16 @@ import (
 	"net/http"
 )
 
+type RequestPayload struct {
+	Action	string			`json:"action"`
+	Auth 	AuthPayload		`json:"auth,omitpay"`
+}
+
+type AuthPayload struct {
+	Email		string	`json:"email"`
+	Password	string	`json:"password"`
+}
+
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:	false,
@@ -21,5 +31,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) HandleSubmission (w http.ResponseWriter, r *http.Request){
-	
+	 
 }
